f3c37a75b9a87e362e97ca5f2b5dfb0bc18cea0d
Farhan Siddiq <157281829+farhansiddiq16@users.noreply.github.com>
2024-02-04 20:04:57 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import inquirer from "inquirer";
+let para = await inquirer.prompt({
+    type: "input",
+    name: "paragrapgh",
+    message: "Enter your paragraph for word count.."
+});
+let words = para.paragrapgh.trim().split(" ");
+console.log(`Your paragrpah word count is ${words.length}`);
